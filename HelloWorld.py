3cc078fa6582378eda91e25cf8689bf14c49d62e
glennpck <glennchia43@gmail.com>
2023-10-04 15:11:36 +0800

    forms+validate user input+url_for

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '9d87de6a296e5fafce4eb599607a9dd4'
 
 posts = [
     {
@@ -24,3 +25,23 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='nagger!')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == "password":
+            flash(f'Log in successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login in Unsuccessful. Please check email and pass', 'danger')
+            
+    return render_template('login.html', title='Login', form=form)

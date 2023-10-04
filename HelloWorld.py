89719e943a30148232347d2bf6bee5f51dc79882
glennpck <glennchia43@gmail.com>
2023-10-04 10:46:36 +0800

    sending dictionary to templates

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,9 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Nnelg Hep',
+        'title': 'Post 1',
+        'content': 'Testing it out',
+        'date_posted': '4 September, 2023'
+    },
+    {
+        'author': 'Trebled Pay',
+        'title': 'Post 2',
+        'content': 'Testing it out again',
+        'date_posted': '4 September, 2023'
+    },
+]
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
